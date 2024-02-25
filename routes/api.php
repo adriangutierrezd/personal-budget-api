7915f6b2b338253048bd5c4c7f0772628c884773
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2024-02-25 17:51:52 +0100

    feat:

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\V1\RecurringController;
 use App\Http\Controllers\Api\V1\RevenueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +22,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'v1',
+], function(){
+
+    Route::post('auth', function(Request $request){
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+    
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+    
+            $basicToken = $user->createToken('basic-token');
+    
+            return [
+                'token' => $basicToken->plainTextToken,
+                'user' => $user
+            ];
+    
+        }
+    });
+
+   
+
 });
 
 Route::group([
