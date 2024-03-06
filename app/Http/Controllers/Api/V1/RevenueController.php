f84f4426d82eba324d26004b8a4e837c24625b62
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2024-03-06 16:08:54 +0100

    Adding correct namespace

--- app/Http/Controllers/Api/V1/RevenueController.php
+++ app/Http/Controllers/Api/V1/RevenueController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\StoreRevenueRequest;
-use App\Http\Requests\UpdateRevenueRequest;
+use App\Http\Requests\V1\StoreRevenueRequest;
+use App\Http\Requests\V1\UpdateRevenueRequest;
 use App\Models\Revenue;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\RevenueCollection;
--- app/Http/Requests/V1/UpdateRevenueRequest.php
+++ app/Http/Requests/V1/UpdateRevenueRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\Policies\V1\RevenuePolicy;
